80d3af2e9031737a2802580ca04c22e80f3aa35d
zengodgr <zengodgr@gmail.com>
2025-09-03 09:31:12 +0300

    dunno!?

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-import withPWA from "next-pwa";
-
-const configWithPWA = withPWA({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
-
-export default configWithPWA;
+export default nextConfig;
